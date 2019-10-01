81598e9f383facbe986571a6f532ff3b7828fb9f
Akshat05-tech <54908649+Akshat05-tech@users.noreply.github.com>
2019-10-01 17:29:54 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,13 @@
 
 public class HelloWorld {
         public static void main(String[] args) {
-                System.out.println("Hello, World!");
+                char A[]=new char[5];
+                char B[]=new char[5];
+                A={'H','E','L','L','O'};
+                B={'W','O','R','L','D'};
+                for(i=0;i<5;i++)
+                        System.out.print(A[i]);
+                for(i=0;i<5;i++)
+                        System.out.print(B[i]);
         }
 }
