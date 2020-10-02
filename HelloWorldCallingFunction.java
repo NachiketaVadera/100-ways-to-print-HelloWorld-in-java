012bbb1029d12da72e7e361f5d3abc1b8b5a2894
ADITI ROY <aditiroy.21.ar@gmail.com>
2020-10-02 19:06:06 +0530

    Calling function that prints character

---
+++ HelloWorldCallingFunction.java
@@ -0,0 +1,22 @@
+
+public class HelloWorldCallingFunction {
+    public static void main(String[] args) {
+        printChar('H');
+        printChar('e');
+        printChar('l');
+        printChar('l');
+        printChar('o');
+        printChar(',');
+        printChar(' ');
+        printChar('W');
+        printChar('o');
+        printChar('r');
+        printChar('l');
+        printChar('d');
+        printChar('!');
+    }
+
+    public static void printChar(char h) {
+        System.out.print(h);
+    }
+}
