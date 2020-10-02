6eeaec5b2452072d23eaeac9a5c9104b5f82c9f2
Endre <decipherino@gmail.com>
2020-10-03 01:24:29 +0200

    esrever ni tub ,dlrow olleh

---
+++ HelloWorld_InReverse.java
@@ -0,0 +1,13 @@
+/**
+ * @author Deki on 03.10.2020
+ * @project 100-ways-to-print-HelloWorld-in-java
+ **/
+public class HelloWorld_InReverse {
+
+  public static void main(String[] args) {
+    StringBuilder builder = new StringBuilder("dlrow olleH");
+    builder.reverse();
+    System.out.println(builder.toString());
+  }
+
+}
