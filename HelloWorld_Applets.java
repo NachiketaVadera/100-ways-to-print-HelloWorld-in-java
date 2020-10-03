2a54b8553dd1ccf226cd9f5920971c867eef4919
Qadry Syed Zubeen <T0245521@gemalto.com>
2020-10-03 21:56:21 +0530

    Hello World using Applets

---
+++ HelloWorld_Applets.java
@@ -0,0 +1,11 @@
+package MyPack;
+
+import java.applet.Applet;  
+import java.awt.Graphics;  
+public class AppletHelloWorld extends Applet{  
+  
+public void paint(Graphics g){  
+g.drawString("Hello World!",50,150);  
+}  
+  
+}  
