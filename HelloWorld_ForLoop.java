7117df2243cd3032c9ed938afed758e0c9f58f85
Navin Nayak <37023798+NayakNavin@users.noreply.github.com>
2020-10-04 13:14:51 +0530

    Create HelloWorld_ForLoop.java

---
+++ HelloWorld_ForLoop.java
@@ -0,0 +1,7 @@
+public class HelloWorld_ForLoop { 
+    public static void main(String args[]) { 
+        for(int i = 0; i < 1; System.out.println("Hello World")) { 
+            i++; 
+        } 
+    } 
+} 
