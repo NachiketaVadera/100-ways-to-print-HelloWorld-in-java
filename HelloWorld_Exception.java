d7820b679e0e08a7dd29aa89f1b5dbd13f408119
Pavan Wadawadagi <pavansdoc@gmail.com>
2020-10-05 13:52:33 +0530

    Print HelloWorld with Exception - Docs

--- HelloWorld_Exception.java
+++ HelloWorld_Exception.java
@@ -1,16 +1,25 @@
-import java.util.function.Function;
-
+/**
+ * The type Hello world exception.
+ */
 public class HelloWorld_Exception {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         print();
     }
 
+    /**
+     * Print Hello World using exception handling
+     */
     private static void print() {
        try {
-           throw new Exception("Hello World");
+           throw new Exception("Hello, World!");
        } catch (Exception ex){
-           System.out.print(ex.getMessage());
+           System.out.println(ex.getMessage());
        }
     }
 
