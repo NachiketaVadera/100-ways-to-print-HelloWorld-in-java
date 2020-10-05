3495c9c1a8e8efdc9dea8b5430a2571ad1cc1750
Aridan <st.aridan@gmail.com>
2020-10-05 19:46:29 +0300

    Print with U+202 character, that overrides visible text direction

---
+++ HelloWorld_Unicode_Right_To_Left_Override.java
@@ -0,0 +1,7 @@
+//Print using unicode U+202 character, that overrides visible text direction
+
+public class HelloWorld_Unicode_Right_To_Left_Override {
+    public static void main(String[] args) {
+        System.out.println("\u202e!dlroW olleH");
+    }
+}
