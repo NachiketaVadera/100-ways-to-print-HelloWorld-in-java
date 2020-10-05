6fd8ce3f31683db0271a32eb18299825cafe2eae
Varun Sharma <coolvarunsharma117@gmail.com>
2020-10-05 21:35:57 +0530

    Update HelloWorld_YourWay.java
    Search a character in String "Hello World" via Linear/Binary search in Java  
    #65

--- HelloWorld_YourWay.java
+++ HelloWorld_YourWay.java
@@ -1,1 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class HelloWorld_YourWay{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a char: ");
+		char c = sc.next().charAt(0);
+		String s = "Hello World";
+		String indexes = "";
+		int i;
+		for(i=0; i<s.length(); i++){
+			if(c == s.charAt(i)){
+				if(indexes.isEmpty()){
+					indexes = indexes+" "+i;
+				}
+				else{
+					indexes = indexes+" and "+i;
+				}
+			}
+		}
+		System.out.println("Char found at index"+ indexes);
+	}
+}
