45be007e0754b979a00c1c42cfea2bc2d3cd9efb
govindraj111 <govindraj7695@gmail.com>
2020-10-06 14:20:44 +0530

    Displaying Hello World using main class default constructor

---
+++ HelloWorld_DefaultConstructor.java
@@ -0,0 +1,16 @@
+/* 
+The code below will display "Hello, World!" using default constructor,
+on standard output.
+*/
+
+public class HelloWorld_DefaultConstructor {
+
+    //constructor
+    public HelloWorld_DefaultConstructor(){
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+            new HelloWorld_DefaultConstructor();
+    }
+}
