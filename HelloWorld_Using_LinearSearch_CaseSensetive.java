4f5b903306f6267bdf767634e472a2aa87565fd7
Varun Sharma <coolvarunsharma117@gmail.com>
2020-10-17 22:14:52 +0530

    Update and rename HelloWorld_Using_LinearSearch(case sensetive!).java to HelloWorld_Using_LinearSearch_CaseSensetive.java

--- HelloWorld_Using_LinearSearch_CaseSensetive.java
+++ HelloWorld_Using_LinearSearch_CaseSensetive.java
@@ -3,7 +3,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class HelloWorld_SearchChar{
+public class HelloWorld_Using_LinearSearch(case sensetive!).java{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a char: ");
